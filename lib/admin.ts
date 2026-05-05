8c9668ee738982381adc910ce907e02f7cb35013
Tkyo0x <gta5azh@gmail.com>
2026-05-04 20:53:37 -0500

    Act

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,6 +1,7 @@
 export const ADMINS = [
   { id: 'user_3D08pnrLjMAqoXqSzTXdrtLfPqX', email: 'jhrodriguez6832@gmail.com' },
   { id: 'user_3D0AaVCGM6j0pt6hpAyH9jOxtvW', email: 'yerikalessandro718@gmail.com' },
+  { id: 'user_3DC4XIn7Bh6Ulc16sfN2NSAoDuo', email: 'obs2cuceps@colsanitas.com' },
 ]
 
 export function isAdmin(userId: string | null): boolean {
