458adb83192d7bc2aede8e6e32b5d91ba4b08a28
Tkyo0x <gta5azh@gmail.com>
2026-05-06 12:40:31 -0500

    fix: abrir endpoint de modulos para la APK

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/app/check-modules(.*)',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
